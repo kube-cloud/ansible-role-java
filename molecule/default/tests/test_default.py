ded95808781cffa914cac09dc2442eb28b6ef474
Jean-Jacques ETUNE NGI <jeanjacques.etunengi@adservio.fr>
2019-07-13 14:34:38 +0200

    Ajustements mineurs

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -8,11 +8,23 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 def test_java_installed(host):
 
+    # Java expected version
+    java_version = '11.0.1'
+
+    # Java expected implementation
+    java_impl = 'openjdk'
+
+    # Java Home Path
+    java_home_path = '/usr/lib/jvm/java-{}-openjdk'.format(java_version)
+
+    # Java archive file
+    java_archive_path = '/tmp/openjdk-{}.tar.gz'.format(java_version)
+
     # Java Home Dirctory
-    java_home = host.file('/usr/lib/jvm/java-11.0.1-openjdk')
+    java_home = host.file(java_home_path)
 
     # Java Downloaded file
-    java_archive = host.file('/tmp/openjdk-11.0.1.tar.gz')
+    java_archive = host.file(java_archive_path)
 
     # Check that Java Archive exists
     assert java_archive.exists
@@ -25,6 +37,9 @@ def test_java_installed(host):
 
     # Check that Java Home is Directory
     assert java_home.is_directory
+
+    # Execute the Java Home Script
+    assert host.run("source /etc/profile.d/java_home.sh").rc == 0
 
     # Run Java version
     java_version_run = host.run('java -version')
@@ -40,13 +55,13 @@ def test_java_installed(host):
     #  java version "1.8.0_161"
     # Second Split result
     #  "1.8.0_161"
-    java_version = java_version_run.stderr.split('\n')[0].split(' ')[2]
+    java_version_ext = java_version_run.stderr.split('\n')[0].split(' ')[2]
 
     # Get Java implementation
-    java_implementation = java_version_run.stderr.split('\n')[0].split(' ')[0]
+    java_impl_ext = java_version_run.stderr.split('\n')[0].split(' ')[0]
 
     # Assert that version is Java 11
-    assert java_version == "\"11.0.1\""
+    assert java_version_ext == "\"{}\"".format(java_version)
 
     # Assert that version is Java implementation is OpenJDK
-    assert java_implementation == "openjdk"
+    assert java_impl_ext == java_impl
